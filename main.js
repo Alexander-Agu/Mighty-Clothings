c11d0739d3b344a56054f0eed96d7f4249639ca1
Alexander-Agu <theonlyagu@gmail.com>
2024-07-05 16:24:23 +0200

    Got the header working

---
+++ main.js
@@ -0,0 +1,10 @@
+// ----- HEADER ----- \\
+const sidebar = document.querySelector(".sidebar");
+
+function closeMenu(){
+    sidebar.style.display = 'none';
+};
+
+function showMenu(){
+    sidebar.style.display = 'flex';
+}
--- script.js
+++ script.js
@@ -1,7 +1,5 @@
-// ----- LOGIN AND SIGN UP PAGE -----
-//...When a user signs up their info will be saved so they could login later
-
-// CONSTANT VARIABLES
+// ----- LOGIN AND SIGN UP PAGE ----- \\
+// CONSTANT VARIABLES FOR THE LOGIN AND SIGN IN PAGE
 const loginButton = document.querySelector(".login-button");
 const signUp = document.getElementById("js-sign");
 const login = document.getElementById("js-login");
@@ -13,10 +11,9 @@ const numberInput = document.getElementById("js-number");
 const passwordInput = document.getElementById("js-password");
 const jsLoginBTN = document.getElementById("js-login");
 
-
 let loginInfo = [];
 
-// When you click on the sign up button you should be directed to the sign up page
+// Handles the signIn link to direct user to from the login to sign up page
 signUp.onclick = ()=>{
     names.textContent = "Create Username";
     number.textContent = "Create Number";
@@ -44,16 +41,15 @@ signUp.onclick = ()=>{
     }
 };
 
-
+// Handles the login button
 login.onclick = ()=>{
     const backToArray = JSON.parse(localStorage.getItem("Detail")); 
     const backToObject = JSON.parse(backToArray);
 
+    // If user enters the correct details he will be directed to the landing page
     if(backToObject.name === nameInput.value && backToObject.number === numberInput.value && backToObject.password === passwordInput.value){
         console.log("Alex" )
-    }
-
-    else{
+    }else{
         alert("Please enter the correct details")
     }
 }
